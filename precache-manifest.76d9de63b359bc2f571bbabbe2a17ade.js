98412e1d70951a5ec88e2dee65c531901b55bfeb
randall5559 <randall5559@gmail.com>
2019-04-11 23:09:06 -0400

    Updates

---
+++ precache-manifest.76d9de63b359bc2f571bbabbe2a17ade.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01d0de1e1df51a88b685",
+    "url": "/user-saver/static/js/runtime~main.f2a1bc54.js"
+  },
+  {
+    "revision": "ad070fc950bee7f7e5b2",
+    "url": "/user-saver/static/js/main.99f589b6.chunk.js"
+  },
+  {
+    "revision": "9e4d08921c556dc49359",
+    "url": "/user-saver/static/js/2.5bb536d8.chunk.js"
+  },
+  {
+    "revision": "ad070fc950bee7f7e5b2",
+    "url": "/user-saver/static/css/main.4cf94f56.chunk.css"
+  },
+  {
+    "revision": "dd1d0581c5832c75f30fd7f9c65242d8",
+    "url": "/user-saver/index.html"
+  }
+];
